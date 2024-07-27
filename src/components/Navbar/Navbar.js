81a8f94e0ae160bb39af4b64f9d5c7f8288d8db9
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-07-28 00:12:34 +0330

    Commit message for client submodule

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png'
 import './Navbar.css'
-import { set } from 'mongoose';
+// import { set } from 'mongoose';
 
 const Navbar = () => {
 
